78bcf87309c1098568555b9c7201adbe737685b2
dhruvilchitroda9 <dhruvilchitroda9@users.noreply.github.com>
2026-04-17 08:04:13 +0000

    chore: remove extra fix/debug files

--- fix_imports.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const f = 'c:/Users/bscom/Desktop/reallll/src/components/cashier/modules/DisbursementDialog.tsx';
-let c = fs.readFileSync(f, 'utf8');
-c = c.replace("import { ScrollArea } from '@/components/ui/scroll-area';\n", '');
-fs.writeFileSync(f, c);
-console.log('Cleaned ScrollArea import');
