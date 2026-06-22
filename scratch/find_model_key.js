e26b502d5bf79e4a78529d7821b4d0cb7a98668b
moneymitra5200 <moneymitra5200@gmail.com>
2026-06-22 12:15:19 +0530

    clean: remove temporary scratch scripts and clean workspace

--- scratch/find_model_key.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const db = new PrismaClient();
-const keys = Object.keys(db).filter(k => !k.startsWith('_'));
-console.log("Keys containing 'borrow' or 'custom' or 'user':");
-console.log(keys.filter(k => k.toLowerCase().includes('borrow') || k.toLowerCase().includes('custom') || k.toLowerCase().includes('user')));
-db.$disconnect();
--- scratch/print_keys.js
+++
@@ -1,4 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const db = new PrismaClient();
-console.log(Object.keys(db).filter(k => !k.startsWith('_')));
-db.$disconnect();
